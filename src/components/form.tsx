de28f3e7a8c7f57ca1ada0390979b751bccf88df
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-20 16:45:44 +0900

    feat: add wireframe for site

---
+++ src/components/form.tsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <>
+      <label for="url">URL</label>
+      <input name="url" id="url" />
+    </>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import Form from './components/form'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: { KV: KVNamespace } }>()
 
 app.use(renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(
+    <>
+      <h1>URL Shortener</h1>
+      <Form />
+    </>
+  )
 })
 
 export default app
