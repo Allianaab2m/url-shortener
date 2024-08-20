bb401178a0c80e8e8f3cde76ff6923ea2ecd05b7
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-20 17:25:45 +0900

    feat: just work

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,8 +1,18 @@
 export default function Form() {
   return (
-    <>
-      <label for="url">URL</label>
-      <input name="url" id="url" />
-    </>
+    <div>
+      <form id="urlForm" method="post" action="/">
+        <label>
+          URL
+          <input type="url" name="url" />
+        </label>
+        {"=>"}
+        <label>
+          Shortened URL
+          <input type="text" name="id" />
+        </label>
+        <button type="submit">Short!!</button>
+      </form>
+    </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,48 @@ const app = new Hono<{ Bindings: { KV: KVNamespace } }>()
 
 app.use(renderer)
 
+app.post("/", async (c) => {
+  const formData = await c.req.formData()
+  const url = formData.get("url")
+  const id = formData.get("id")
+
+  if (typeof url === "string" && typeof id === "string") {
+    const value = await c.env.KV.get(id)
+    if (value) {
+      return c.redirect("/?error=true&reason=duplicate")
+    }
+
+    await c.env.KV.put(id, url)
+
+    return c.redirect("/")
+  }
+
+  return c.redirect("/?error=true")
+})
+
 app.get('/', (c) => {
+  const { error, reason } = c.req.query()
   return c.render(
     <>
       <h1>URL Shortener</h1>
       <Form />
+      {error && <div>Error occurred...</div>}
+      {reason && <div>Reason: {reason}</div>}
+    </>
+  )
+})
+
+app.get("/:key", async (c) => {
+  const { key } = c.req.param()
+  const value = await c.env.KV.get(key)
+
+  if (value) {
+    return c.redirect(value)
+  }
+
+  return c.render(
+    <>
+      <h1>404 Not found</h1>
     </>
   )
 })
